3e7cc736d408b589e0a355539f1adda8ff70e258
rogeto20 <rogeto20@jcu.edu>
2019-03-11 16:40:23 -0400

    Made changes to make the webpages viewable

---
+++ Telestrations/Telestrations/Controllers/GameSetupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Telestrations.Controllers
+{
+    public class GameSetupController : Controller
+    {
+        // GET: GameSetup
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Preview()
+        {
+            return View();
+        }
+    }
+}
---
+++ Telestrations/Telestrations/Controllers/PlayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Telestrations.Controllers
+{
+    public class PlayController : Controller
+    {
+        // GET: Play
+        public ActionResult Play()
+        {
+            return View();
+        }
+        public ActionResult Results()
+        {
+            return View();
+        }
+    }
+}
