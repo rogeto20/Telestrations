e81a52e394e856cc14a72f152f6624c181cb5563
rogeto20 <rogeto20@jcu.edu>
2019-03-23 16:37:57 -0400

    Added a new solution

---
+++ Project2/Project2/Controllers/PlayController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2.Controllers
+{
+    public class PlayController : Controller
+    {
+        // GET: Play
+        public ActionResult Play()
+        {
+            return View();
+        }
+        public ActionResult Results()
+        {
+            return View();
+        }
+        public ActionResult Players()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project2/Project2/Controllers/SetupController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2.Controllers
+{
+    public class SetupController : Controller
+    {
+        // GET: Setup
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Preview()
+        {
+            return View();
+        }
+    }
+}
