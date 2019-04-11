1ee93d47edf4324156ef077e07c0040f184bcfdc
rogeto20 <rogeto20@jcu.edu>
2019-04-11 16:06:21 -0400

    This version works without the drawing panel!

--- Project2/Project2/Controllers/SetupController.cs
+++ Project2/Project2/Controllers/SetupController.cs
@@ -1,23 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Project2.Models;
 
 namespace Project2.Controllers
 {
     public class SetupController : Controller
     {
+        private Project db = new Project();
+
         // GET: Setup
-        public ActionResult About()
+        public ActionResult Index()
         {
-            return View();
+            return View(db.Games.ToList());
         }
+
+        // GET: Setup/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GameViewModel gameViewModel = db.Games.Find(id);
+            if (gameViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gameViewModel);
+        }
+
+        // GET: Setup/Create
         public ActionResult Create()
         {
             return View();
         }
-        public ActionResult Preview()
+
+        // POST: Setup/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,owner,category,numPlayers,name")] GameViewModel gameViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(gameViewModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gameViewModel);
+        }
+
+        // GET: Setup/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GameViewModel gameViewModel = db.Games.Find(id);
+            if (gameViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gameViewModel);
+        }
+
+        // POST: Setup/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,owner,category,numPlayers,name")] GameViewModel gameViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gameViewModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gameViewModel);
+        }
+
+        // GET: Setup/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GameViewModel gameViewModel = db.Games.Find(id);
+            if (gameViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gameViewModel);
+        }
+
+        // POST: Setup/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GameViewModel gameViewModel = db.Games.Find(id);
+            db.Games.Remove(gameViewModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        // GET: Setup/Create
+        public ActionResult About()
         {
             return View();
         }
---
+++ Project2/Project2/Models/GameViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project2.Models
+{
+    public class Project : DbContext
+    {
+        public DbSet<GameViewModel> Games { get; set; }
+    }
+
+    public class GameViewModel
+    {
+        public int ID { get; set; }
+        public string owner { get; set; }
+        public string category { get; set; }
+        public int numPlayers { get; set; }
+        public string name { get; set; }
+    }
+
+    
+}
